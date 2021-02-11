560aec2d544c0f7336d3c29cba3bb048f4ce4358
Christopher <onlygamernerd@gmail.com>
2021-02-11 01:18:29 +0100

    use recoil

--- web/src/app.js
+++ web/src/app.js
@@ -1,39 +1,15 @@
-import '@babel/polyfill';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+
+
+import { RecoilRoot } from 'recoil';
 
 import App from 'containers/App';
 
-import WindowListener from 'containers/WindowListener';
-import Screen from "./components/screen/screen";
+ReactDOM.render(
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>,
+  document.getElementById('root')
+);
 
-import configureStore from './configureStore';
-import HeaderComponent from "./components/header";
-
-const initialState = {};
-const store = configureStore(initialState);
-const MOUNT_NODE = document.getElementById('app');
-
-const render = () => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <WindowListener>
-        <>
-          <App />
-        </>
-      </WindowListener>
-    </Provider>,
-    MOUNT_NODE,
-  );
-};
-
-if (module.hot) {
-  module.hot.accept(['containers/App'], () => {
-    ReactDOM.unmountComponentAtNode(MOUNT_NODE);
-    render();
-  });
-}
-
-render();
