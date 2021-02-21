e96b9170558bdaebebe241aee0d955a99b276100
Christopher <onlygamernerd@gmail.com>
2021-02-21 22:44:30 +0100

    use themeProvider

---
+++ web/src/Bank.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Container from './components/container/Container';
+import { ThemeProvider } from '@material-ui/core';
+import { useTheme } from './theme/useTheme';
+import Header from './components/header/Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/home/components/Home';
+
+function Bank() {
+
+  const currentTheme = useTheme()
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <Container>
+        <Router>
+          <>
+          <Header />
+          <Switch>
+            <Route path='/' exact component={Home}/>
+          </Switch>
+          </>
+        </Router>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default Bank;
