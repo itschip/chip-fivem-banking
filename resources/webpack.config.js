c196be30e00b7efc1bbf115fde6fa8d1de6d81ee
Christopher <onlygamernerd@gmail.com>
2021-02-28 21:06:46 +0100

    inital fivem code

---
+++ resources/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require("path");
+const webpack = require("webpack");
+const RemovePlugin = require("remove-files-webpack-plugin");
+
+const buildPath = path.resolve(__dirname, "dist");
+
+const client = {
+  entry: "client/client.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new RemovePlugin({
+      before: {
+        include: [path.resolve(buildPath, "client")],
+      },
+      watch: {
+        include: [path.resolve(buildPath, "client")],
+      },
+    }),
+  ],
+};
+
+const server = {
+  entry: "server/server.ts",
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({ "global.GENTLY": false }),
+    new RemovePlugin({
+      before: {
+        include: [path.resolve(buildPath, "server")],
+      },
+      watch: {
+        include: [path.resolve(buildPath, "server")],
+      },
+    }),
+  ],
+  optimization: {
+    minimize: true,
+  },
+  resolve: {
+    extensions: ["*.ts", "*.tsx", "*.js"],
+  },
+  output: {
+    filename: "[cotenthash].server.js",
+    path: path.resolve(buildPath, "server"),
+  },
+  target: "node",
+};
