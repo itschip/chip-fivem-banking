1783b6ce3b986e6068d46546f4115aae6d4d60e5
Christopher <onlygamernerd@gmail.com>
2021-03-03 11:15:54 +0100

    feat(resources/server): db utils

---
+++ resources/server/db.ts
@@ -0,0 +1,10 @@
+import mysql from 'mysql2';
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  port: parseInt(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+});
+
+export const promisePool = pool.promise();
