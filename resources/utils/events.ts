0f01620e8d9ad6199b6c6fd5d12b8deb5e68f7f1
Christopher <onlygamernerd@gmail.com>
2021-03-03 11:15:24 +0100

    feat(resources/utils): functions for bank actions

---
+++ resources/utils/events.ts
@@ -0,0 +1,4 @@
+export default {
+  BANK_DEPOSIT_MONEY: 'nbwd:depositMoney',
+  BANK_GET_CREDENTIALS: 'nbwd:getCredentials',
+};
--- resources/utils/fivem.ts
+++ resources/utils/fivem.ts
@@ -1,1 +1,11 @@
 export const Delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+export const sendMessage = (method: string, data = {}) => {
+  return SendNuiMessage(
+    JSON.stringify({
+      app: 'NBWD',
+      method: method,
+      data: data,
+    }),
+  );
+};
