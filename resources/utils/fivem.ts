198f1967ab65fb5911350960a8cb0a17eafc6d99
Christopher <onlygamernerd@gmail.com>
2021-03-07 21:40:31 +0100

    euclidean distance check

--- resources/utils/fivem.ts
+++ resources/utils/fivem.ts
@@ -9,3 +9,12 @@ export const sendMessage = (method: string, data = {}) => {
     }),
   );
 };
+
+function getDistance(player: number[], target: number[]) {
+  return (
+    player
+      .map((x, i) => Math.abs(x - target[i]) ** 2) // square the difference
+      .reduce((sum, now) => sum + now) ** // sum
+    (1 / 2)
+  );
+}
